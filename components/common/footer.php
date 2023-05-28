4b6c20e0b3ed491b4f1f1bd85e3de7485b7121a9
jurco-exe <jurajbosy1@gmail.com>
2023-05-28 17:28:25 +0200

    feat: hruba stavba home stranky

---
+++ components/common/footer.php
@@ -0,0 +1,9 @@
+<footer class="container pt-5">
+  <ul class="d-flex justify-content-between align-items-center mt-5">
+    <li><a href="/">Home</a></li>
+    <li><a href="/">Shop</a></li>
+    <li><a href="/">About</a></li>
+    <li><a href="/">Updates</a></li>
+    <li><a href="/">Contact</a></li>
+  </ul>
+</footer>
---
+++ components/common/navbar.php
@@ -0,0 +1,13 @@
+<div class="container-fluid px-5 d-flex justify-content-between">
+  <div class="navbar-logo d-flex justify-content-center align-items-center pt-2">
+    <h2>HUGO BOSS</h2>
+  </div>
+  <div class="navbar-links d-flex justify-content-center align-items-center pt-4">
+    <ul class="d-flex list-unstyled gap-5">
+      <li><a href="/">Home</a></li>
+      <li><a href="../screens/shop.php">Shop</a></li>
+      <li><a href="../screens/about.php">About</a></li>
+      <li><a href="../screens/contact.php">Contact</a></li>
+    </ul>
+  </div>
+</div>
