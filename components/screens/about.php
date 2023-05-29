e7b5cbffda4487c65a3e6f90a143c037a1e5c7c8
jurco-exe <jurajbosy1@gmail.com>
2023-05-29 23:22:07 +0200

    feat: about done

---
+++ components/screens/about.php
@@ -0,0 +1,71 @@
+<?php @include './components/common/header.php' ?>
+
+<body class="about-class">
+  <div class="container-fluid px-5 d-flex justify-content-between mb-5 navbar position-fixed about-navbar">
+    <div class="navbar-logo d-flex justify-content-center align-items-center pt-2">
+      <h2>HUGO BOSS</h2>
+    </div>
+    <div class="navbar-links d-flex justify-content-center align-items-center pt-4">
+      <ul class="d-flex list-unstyled gap-5">
+        <li><a href="/">Home</a></li>
+        <li><a href="../screens/shop.php">Shop</a></li>
+        <li><a href="../screens/about.php">About</a></li>
+        <li><a href="../screens/contact.php">Contact</a></li>
+      </ul>
+    </div>
+  </div>
+
+  <section id="about-us" class="py-5 my-5">
+    <div class="container">
+      <div class="row ">
+        <!-- <div class="col"> -->
+        <h3>
+          ABOUT US
+        </h3>
+        <p>Set up in 1994, ‘Organization Name’ is the US’s biggest free, aroma retailer.</p>
+        <p>Our point is to make standard and extravagance scent moderate and open to all. We grandstand more than 130 aroma marks, which are all accessible for you to peruse online at your relaxation. Remember our breathtaking determinations in-store as well!</p>
+        <p>We highly esteem conveying remarkable support of our clients, we have as of late been granted for magnificent client benefit.</p>
+        <p>Regardless of whether you shop in one of more than 200 stores, on the web, or via telephone our well-disposed group are close by to help make finding the ideal aroma a pleasurable ordeal for all. </p>
+        <!-- </div> -->
+      </div>
+      <div class="row gridik">
+        <div class="column">
+          <img src="./assets/img/aboutus1.jpg" alt="Image 1" class="img-thumbnail" height="800">
+        </div>
+        <div class="column">
+          <img src="./assets/img/aboutus2.jpg" alt="Image 1" class="img-thumbnail" height="800">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="headquarter" class="py-5">
+    <div class="container">
+      <div class="wrapper">
+        <div class="left-col">
+          <h3>OUR HEADQUARTER</h3>
+          <p>Corporate headquarters of Hugo Boss , located in Cupertino, California, United States. It was opened to employees in April 2010, while construction was still underway, and superseded the original headquarters at 1 Infinite Loop, which opened in 1993.</p>
+        </div>
+        <div class="right-col">
+          <img src="./assets/img/quarter.jpg" alt="Image 1" class="img-thumbnail" height="800">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="our-designs" class="py-5">
+    <div class="container">
+      <h3 class="text-center mb-5">OUR DESIGNS</h3>
+      <div class="wrapper">
+        <div class="container-left-long"><img src="./assets/img/container-left.jpg" alt="Image 1" class="img-thumbnail"></div>
+        <div class="container-right-up"><img src="./assets/img/container-right-up.jpg" alt="Image 1" class="img-thumbnail"></div>
+        <div class="container-right-down"><img src="./assets/img/container-right-down.jpg" alt="Image 1" class="img-thumbnail"></div>
+      </div>
+    </div>
+  </section>
+
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</body>
+
+</html>
