6f086ad3013b059f9bce8096b5654a0418ff41fe
jurco-exe <jurajbosy1@gmail.com>
2023-06-05 17:34:38 +0200

    feat: porobeny about

--- index.php
+++ index.php
@@ -1,74 +1,87 @@
 <?php @include './components/common/header.php' ?>
 
-<body class="about-class">
-	<div class="container-fluid px-5 d-flex justify-content-between mb-5 navbar position-fixed about-navbar">
-		<div class="navbar-logo d-flex justify-content-center align-items-center pt-2">
-			<a href="../../index.php">
-				<h2>HUGO BOSS</h2>
-			</a>
-		</div>
-		<div class="navbar-links d-flex justify-content-center align-items-center pt-3">
-			<ul class="d-flex list-unstyled gap-5">
-				<li><a href="/">Home</a></li>
-				<li><a href="../screens/shop.php">Shop</a></li>
-				<li><a href="../screens/about.php" class="active">About</a></li>
-				<li><a href="../screens/contact.php">Contact</a></li>
-			</ul>
-		</div>
-	</div>
+<body>
+	<section id="hero-section">
+		<?php @include './components/common/navbar.php' ?>
+	</section>
 
-	<section id="about-us" class="py-5 my-5">
+	<section id="premium-collection" class="py-5">
 		<div class="container">
-			<div class="row ">
-				<!-- <div class="col"> -->
-				<h3>
-					ABOUT US
-				</h3>
-				<p>Set up in 1994, ‘Organization Name’ is the US’s biggest free, aroma retailer.</p>
-				<p>Our point is to make standard and extravagance scent moderate and open to all. We grandstand more than 130 aroma marks, which are all accessible for you to peruse online at your relaxation. Remember our breathtaking determinations in-store as well!</p>
-				<p>We highly esteem conveying remarkable support of our clients, we have as of late been granted for magnificent client benefit.</p>
-				<p>Regardless of whether you shop in one of more than 200 stores, on the web, or via telephone our well-disposed group are close by to help make finding the ideal aroma a pleasurable ordeal for all. </p>
-				<!-- </div> -->
+			<div class="text py-5">
+				<h3 class="text-center mb-5">Premium Collection</h3>
+				<p class="text-center mt-5">Free shipping with orders over $400</p>
 			</div>
-			<div class="row gridik">
-				<div class="column1">
-					<img src="./assets/img/aboutus1.jpg" alt="Image 1" class="img-thumbnail" height="800">
+			<div class="row px-5 my-5">
+				<div class="col-md-4 column-image">
+					<img src="./assets/img/premium-1.jpg" alt="Image 1" class="img-thumbnail" height="800">
 				</div>
-				<div class="column2">
-					<img src="./assets/img/aboutus2.jpg" alt="Image 1" class="img-thumbnail" height="800">
+				<div class="col-md-4 column-image">
+					<img src="./assets/img/premium-2.jpg" alt="Image 2" class="img-thumbnail" height="800">
+				</div>
+				<div class="col-md-4 column-image">
+					<img src="./assets/img/premium-3.jpg" alt="Image 3" class="img-thumbnail" height="800">
 				</div>
 			</div>
 		</div>
 	</section>
 
-	<section id="headquarter" class="py-5">
+	<section id="our-philosofy" class="py-5 d-flex justify-content-center align-items-center">
 		<div class="container">
-			<div class="wrapper px-5 ">
-				<div class="left-col">
-					<h3>OUR HEADQUARTER</h3>
-					<hr>
-					<p>Corporate headquarters of Hugo Boss , located in <br> Cupertino, California, United States. It was opened to <br> employees in April 2010, while construction was still <br> underway, and superseded the original headquarters at 1 <br> Infinite Loop, which opened in 1993.</p>
+			<div class="text py-5">
+				<h1 class="text-center mb-5">OUR PHILOSOPHY</h1>
+				<p class="text-center mt-5">We have produced the perfect perfume with the best scent and quality in the world</p>
+			</div>
+		</div>
+	</section>
+
+	<section id="featured-products" class="py-5">
+		<div class="container">
+			<div class="row text-center d-flex gap-3 mb-5">
+				<h1>Featured products</h1>
+				<p>Includes free shipping (US only)</p>
+			</div>
+			<div class=" wrapper">
+				<div class="card c1 ">
+					<div class="card-header">
+						<img src="./assets/img/hugo-boss-the-scent-2.jpg" alt="Image 3" class="img-thumbnail" height="800">
+					</div>
+					<div class="card-body">
+						<h3>BOSS Rebruta</h3>
+						<a href="">VIEW PRODUCT</a>
+					</div>
 				</div>
-				<div class="right-col">
-					<img src="./assets/img/quarter.jpg" alt="Image 1" class="img-thumbnail" height="800">
+				<div class="card c2">
+					<div class="card-header">
+						<img src="./assets/img/hugo-boss-the-scent.jpg" alt="Image 3" class="img-thumbnail" height="800">
+					</div>
+					<div class="card-body">
+						<h3>HUGO Fiora</h3>
+						<a href="">VIEW PRODUCT</a>
+					</div>
+				</div>
+				<div class="card c3">
+					<div class="card-header">
+						<img src="./assets/img/hugo-boss-night.jpg" alt="Image 3" class="img-thumbnail" height="800">
+					</div>
+					<div class="card-body">
+						<h3>BOSS Hugo</h3>
+						<a href="">VIEW PRODUCT</a>
+					</div>
 				</div>
 			</div>
 		</div>
 	</section>
 
-	<section id="our-designs" class="py-5">
-		<div class="container">
-			<h3 class="text-center mb-5">OUR DESIGNS</h3>
-			<div class="wrapper">
-				<div class="container-left-long"><img src="./assets/img/container-left.jpg" alt="Image 1" class="img-thumbnail"></div>
-				<div class="container-right-up"><img src="./assets/img/container-right-up.jpg" alt="Image 1" class="img-thumbnail"></div>
-				<div class="container-right-down"><img src="./assets/img/container-right-down.jpg" alt="Image 1" class="img-thumbnail"></div>
-			</div>
-		</div>
+	<section id="tag" class="d-flex justify-content-center align-items-center gap-3 flex-column">
+		<a href="/">This Weekend</a>
+		<h1>UP TO 15% OFF ON ALL PRODUCTS</h1>
+		<p>Now Through December 31</p>
 	</section>
 
+	<?php @include './components/common/footer.php' ?>
 
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+	<script src="../../app.js"></script>
 </body>
 
 </html>
